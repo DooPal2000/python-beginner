4906d1209c7e122dc00757785aba53b58cc87d36
Kyoung Yeob <kkyub1313@gmail.com>
2025-07-06 13:42:39 +0900

    feat(movie): Actor 모델, 마이그레이션, serializer 추가

--- drf-movie-api/movie_api/movies/urls.py
+++ drf-movie-api/movie_api/movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import movie_list
+from .views import *
 
 urlpatterns = [
-    path('movies', movie_list),    
+    path('movies', movie_list),  
+    path('actor', actor_list)  
 ]
